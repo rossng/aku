4152ea94adbdc088d2568cea9e0791786654d454
Ross Gardiner <ross-public@gmx.com>
2017-11-21 11:40:47 +0000

    Get simple debugger repl working

--- aku-debugger/app/Main.hs
+++ aku-debugger/app/Main.hs
@@ -2,6 +2,9 @@
 module Main where
 
 import Data.Void
+import Data.Maybe
+import Control.Monad
+import Control.Monad.Loops
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Expr
@@ -11,20 +14,30 @@ import CPU
 type Parser = Parsec Void String
 
 data Command =  Reset -- resets the CPU to initialCPU
+              | Quit
+              deriving (Eq, Show)
 
 main :: IO ()
 main = do   putStrLn "aku debugger"
-            let cpu = initialCPU
-            commandString <- getLine
-            let command = parseMaybe commandParser commandString
-            cpu <- case command of
-                    Just cmd    -> return (applyCommand cmd cpu)
-                    Nothing     -> do { putStrLn "Command not recognised"; return cpu }
-            print cpu
+            finalCPU <- iterateUntilM ((== Just Quit) . snd) repl (initialCPU, Nothing)
+            print finalCPU
+            return ()
+
+repl :: (CPU, Maybe Command) -> IO (CPU, Maybe Command)
+repl (cpu, command) = do
+        print cpu
+        commandString <- getLine
+        cpu' <- case command of
+               Just cmd    -> return (applyCommand cmd cpu)
+               Nothing     -> return cpu
+        let command' = parseMaybe commandParser commandString
+        when (isNothing command') $ putStrLn "Command not recognised"
+        return (cpu', command')
 
 applyCommand :: Command -> CPU -> CPU
 applyCommand command cpu = case command of
                                 Reset   -> initialCPU
+                                Quit    -> cpu
 
 sc :: Parser ()
 sc = L.space space1 lineCmnt blockCmnt
@@ -52,6 +65,8 @@ quoted = between (symbol "\"") (symbol "\"")
 resetParser :: Parser Command
 resetParser =     Reset <$ try (rword "reset")
               <|> Reset <$ try (rword "r")
+              <|> Quit <$ try (rword "quit")
+              <|> Quit <$ try (rword "q")
 
 commandParser :: Parser Command
 commandParser = resetParser <* eof
